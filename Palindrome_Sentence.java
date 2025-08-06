09b983bd613e248e36373b7d87477cec9df88a16
devchowdary <madaladevendra16@gmail.com>
2025-08-06 13:51:12 +0530

    2025-08-05 gfg  daily problem solution added

---
+++ Palindrome_Sentence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalinSent(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        String cleaned = sb.toString();
+        int i = 0, j = cleaned.length() - 1;
+        while (i < j) {
+            if (cleaned.charAt(i) != cleaned.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
